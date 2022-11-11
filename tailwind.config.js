ecc555405336afe75e498f15357d390a4383d43e
Naveed Shahzad <lhrciit@gmail.com>
2022-11-11 18:07:35 +0500

    add slider into code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-     require('@tailwindcss/typography'),
-     require('@tailwindcss/forms'),
-  ],
+	content: [
+		"./src/**/*.{html,ts}",
+	],
+  darkMode: 'class',
+  important: true,
+	theme: {
+		 screens: {
+						xs: "540px",
+						sm: '640px',
+						md: '768px',
+						lg: '1024px',
+						xl: '1280px',
+						'2xl': '1536px',
+				},
+
+		container: {
+				center: true,
+				padding: {
+						DEFAULT: '12px',
+						sm: '1rem',
+						lg: '45px',
+						xl: '5rem',
+						'2xl': '13rem',
+				},
+
+		},
+		extend: {
+      colors: {
+                'primary': '#f78e2f',
+                'primary-dark': '#e77e1f',
+            },
+      zIndex: {
+                1: '1',
+                2: '2',
+                3: '3',
+                999: '999',
+            },
+		},
+	},
+	plugins: [
+		 require('@tailwindcss/typography'),
+		 require('@tailwindcss/forms'),
+	],
 }
