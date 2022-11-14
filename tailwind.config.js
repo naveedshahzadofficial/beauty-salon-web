766cd7a51f4027cc2e10efb68bab8cd12a1b09e5
Naveed Shahzad <lhrciit@gmail.com>
2022-11-14 17:44:50 +0500

    working on home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,17 @@ module.exports = {
 			  fontFamily: {
         montserrat: ['Montserrat', 'serif'],
         playfairDisplay: [ 'Playfair Display', 'serif'],
-      }, 
+        atcripley:['ATC Ripley', 'sans-serif'],
+      },
+        boxShadow: {
+                sm: '0 2px 4px 0 rgb(60 72 88 / 0.15)',
+                DEFAULT: '0 0 3px rgb(60 72 88 / 0.15)',
+                md: '0 5px 13px rgb(60 72 88 / 0.20)',
+                lg: '0 10px 25px -3px rgb(60 72 88 / 0.15)',
+                xl: '0 20px 25px -5px rgb(60 72 88 / 0.1), 0 8px 10px -6px rgb(60 72 88 / 0.1)',
+                '2xl': '0 25px 50px -12px rgb(60 72 88 / 0.25)',
+                inner: 'inset 0 2px 4px 0 rgb(60 72 88 / 0.05)',
+            },
       zIndex: {
                 1: '1',
                 2: '2',
