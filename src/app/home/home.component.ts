64880fb0087c780248e66beaced8594ba2bacbf8
Naveed Shahzad <lhrciit@gmail.com>
2022-11-14 19:54:47 +0500

    app installation help has been complted

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,17 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  help_cards = [
+    {
+      "title":"Download & Install",
+      "image_file":"assets/images/how-use-app-1.png",
+    },{
+      "title":"Book Online",
+      "image_file":"assets/images/how-use-app-2.jpg",
+    },{
+      "title":"Rate Our Services",
+      "image_file":"assets/images/how-use-app-3.jpg",
+    }
+  ];
+
 }
