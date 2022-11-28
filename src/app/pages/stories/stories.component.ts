39326eaef0254a85339a2afa7c48c3c1138bd06b
Naveed Shahzad <lhrciit@gmail.com>
2022-11-28 18:45:37 +0500

    formate the code

--- src/app/pages/stories/stories.component.ts
+++ src/app/pages/stories/stories.component.ts
@@ -7,53 +7,55 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stories',
   templateUrl: './stories.component.html',
-  styleUrls: ['./stories.component.scss']
+  styleUrls: ['./stories.component.scss'],
 })
 export class StoriesComponent implements OnInit {
-
   stories: IStory[] = [];
   page: number = 1;
   per_page: number = 10;
   meta!: IMeta;
   links!: ILinks;
 
-  constructor(private pageService:PageService) { }
+  constructor(private pageService: PageService) {}
 
   ngOnInit(): void {
     this.getStories();
   }
 
-  getStories(){
-    this.pageService.storiesPage(this.per_page, this.page).subscribe((resp:any) =>{
-      console.log(resp);
-      this.stories = resp.data;
-      this.links = resp.links;
-      this.meta = resp.meta;
-      this.meta.links = this.meta.links.map(link => {
-        if(link.label === '&laquo; Previous'){
-          link.label = '«';
-        }else if(link.label === 'Next &raquo;')
-        {
-          link.label = '»';
-        }
-        if(link.url){
-         let url = new URL(link.url);
-         let page = url.searchParams.get("page");
-         link.url = page?.toString();
-        }
-        return link;
+  getStories() {
+    this.pageService
+      .storiesPage(this.per_page, this.page)
+      .subscribe((resp: any) => {
+        console.log(resp);
+        this.stories = resp.data;
+        this.links = resp.links;
+        this.meta = resp.meta;
+        this.meta.links = this.meta.links.map((link) => {
+          if (link.label === '&laquo; Previous') {
+            link.label = '«';
+          } else if (link.label === 'Next &raquo;') {
+            link.label = '»';
+          }
+          if (link.url) {
+            let url = new URL(link.url);
+            let page = url.searchParams.get('page');
+            link.url = page?.toString();
+          }
+          return link;
+        });
       });
-    });
   }
 
-  pageChange(page: string){
+  pageChange(page: string) {
     this.page = parseInt(page);
     this.getStories();
   }
   scroll(el: HTMLElement) {
     console.log(el);
-    el.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
- }
-
-
+    el.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+  }
 }
